78c3c34a7c4e588bdecd7fa87ab32fe63b89728c
Gustavo Corrado <gustavocorrado98@gmail.com>
2020-07-06 06:57:04 -0300

    [UPDATE]
    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace APSTextEncrypter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EncryptBtn = new System.Windows.Forms.Button();
+            this.DecryptBtn = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.créditosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.informaçãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // EncryptBtn
+            // 
+            this.EncryptBtn.Location = new System.Drawing.Point(12, 92);
+            this.EncryptBtn.Name = "EncryptBtn";
+            this.EncryptBtn.Size = new System.Drawing.Size(117, 48);
+            this.EncryptBtn.TabIndex = 0;
+            this.EncryptBtn.Text = "Encrypt Text";
+            this.EncryptBtn.UseVisualStyleBackColor = true;
+            this.EncryptBtn.Click += new System.EventHandler(this.EncryptBtn_Click);
+            // 
+            // DecryptBtn
+            // 
+            this.DecryptBtn.Location = new System.Drawing.Point(135, 92);
+            this.DecryptBtn.Name = "DecryptBtn";
+            this.DecryptBtn.Size = new System.Drawing.Size(128, 48);
+            this.DecryptBtn.TabIndex = 1;
+            this.DecryptBtn.Text = "Decrypt Text";
+            this.DecryptBtn.UseVisualStyleBackColor = true;
+            this.DecryptBtn.Click += new System.EventHandler(this.DecryptBtn_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(275, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.créditosToolStripMenuItem,
+            this.informaçãoToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(44, 20);
+            this.toolStripMenuItem1.Text = "Help";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // créditosToolStripMenuItem
+            // 
+            this.créditosToolStripMenuItem.Name = "créditosToolStripMenuItem";
+            this.créditosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.créditosToolStripMenuItem.Text = "Credits";
+            this.créditosToolStripMenuItem.Click += new System.EventHandler(this.créditosToolStripMenuItem_Click);
+            // 
+            // informaçãoToolStripMenuItem
+            // 
+            this.informaçãoToolStripMenuItem.Name = "informaçãoToolStripMenuItem";
+            this.informaçãoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.informaçãoToolStripMenuItem.Text = "Info";
+            this.informaçãoToolStripMenuItem.Click += new System.EventHandler(this.informaçãoToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Warning!";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Do not use this software on important files!";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 68);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(198, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "It was made for educational purposes only.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 152);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DecryptBtn);
+            this.Controls.Add(this.EncryptBtn);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "APSTextEncryptor 1.0";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button EncryptBtn;
+        private System.Windows.Forms.Button DecryptBtn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem créditosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem informaçãoToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace APSTextEncrypter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void EncryptBtn_Click(object sender, EventArgs e)
+        {
+            var choosenFile = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                string fileContent = "";
+                string fileName = "";
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "File TXT(*.txt)|*.txt*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    fileContent = openFileDialog.FileName;
+                    fileName = openFileDialog.SafeFileName;
+                }
+
+                if (File.Exists(fileContent))
+                {
+                    string encryptStr = "";
+                    string password = "1337";
+                    using (StreamReader sr = new StreamReader(fileContent))
+                    {
+                        encryptStr = sr.ReadToEnd();
+                    }
+                    encryptStr = EncryptText(encryptStr, password);
+                    WriteFile(encryptStr, fileName);
+
+                }
+            }
+        }
+
+        private void DecryptBtn_Click(object sender, EventArgs e)
+        {
+            var choosenFile = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                string fileContent = "";
+                string fileName= "";
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "File TXT(*.txt)|*.txt*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    fileContent = openFileDialog.FileName;
+                    fileName = openFileDialog.SafeFileName;
+                }
+
+                if (File.Exists(fileContent))
+                {
+                    string decryptStr = "";
+                    string password = "1337";
+                    using (StreamReader sr = new StreamReader(fileContent))
+                    {
+                        decryptStr = sr.ReadToEnd();
+                    }
+                    decryptStr = DecryptText(decryptStr, password);
+                    WriteFile(decryptStr, fileName);
+
+                }
+
+            }
+        }
+        
+        public string EncryptText(string textData, string Encryptionkey)
+        {
+
+            RijndaelManaged objrij = new RijndaelManaged();
+            objrij.Mode = CipherMode.CBC;
+            objrij.Padding = PaddingMode.PKCS7;
+            objrij.KeySize = 0x80;
+            objrij.BlockSize = 0x80;
+            byte[] passBytes = Encoding.UTF8.GetBytes(Encryptionkey);
+            byte[] EncryptionkeyBytes = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            int len = passBytes.Length;
+            if (len > EncryptionkeyBytes.Length)
+            {
+                len = EncryptionkeyBytes.Length;
+            }
+            Array.Copy(passBytes, EncryptionkeyBytes, len);
+            objrij.Key = EncryptionkeyBytes;
+            objrij.IV = EncryptionkeyBytes;
+            ICryptoTransform objtransform = objrij.CreateEncryptor();
+            byte[] textDataByte = Encoding.UTF8.GetBytes(textData);
+            return Convert.ToBase64String(objtransform.TransformFinalBlock(textDataByte, 0, textDataByte.Length));
+        }
+
+        string DecryptText(string EncryptedText, string Encryptionkey)
+        {
+            RijndaelManaged objrij = new RijndaelManaged();
+            objrij.Mode = CipherMode.CBC;
+            objrij.Padding = PaddingMode.PKCS7;
+            objrij.KeySize = 0x80;
+            objrij.BlockSize = 0x80;
+            byte[] encryptedTextByte = Convert.FromBase64String(EncryptedText);
+            byte[] passBytes = Encoding.UTF8.GetBytes(Encryptionkey);
+            byte[] EncryptionkeyBytes = new byte[0x10];
+            int len = passBytes.Length;
+            if (len > EncryptionkeyBytes.Length)
+            {
+                len = EncryptionkeyBytes.Length;
+            }
+            Array.Copy(passBytes, EncryptionkeyBytes, len);
+            objrij.Key = EncryptionkeyBytes;
+            objrij.IV = EncryptionkeyBytes;
+            byte[] TextByte = objrij.CreateDecryptor().TransformFinalBlock(encryptedTextByte, 0, encryptedTextByte.Length);
+            return Encoding.UTF8.GetString(TextByte);  //it will return readable string
+        }
+
+        private static byte[] Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+
+            var saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        private static byte[] Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+            var saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+
+                    decryptedBytes = ms.ToArray();
+                }
+            }
+
+            return decryptedBytes;
+        }
+
+        public void WriteFile(string contents, string fileName)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)+"\\"+fileName;
+            MessageBox.Show(path);
+            File.WriteAllText(path, contents);
+            string readText = File.ReadAllText(path);
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void créditosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string credits = +"Gustavo Corrado"
+                             +"Information Security college project\n" +
+                             "Information Systems - 4 semester.\n"/
+            MessageBox.Show(credits, "Credits");
+        }
+
+        private void informaçãoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string info = "APSTextEncrypter 1.0\n"
+                            + "Made using C# (.NET Framework) for Windows Platforms\n"
+                            +"Encrypted and decrypted files are located in the Documents folder;";
+            MessageBox.Show(info, "About");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
